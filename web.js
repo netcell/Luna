b20a51b4b61e1e608f74b0c39b9d4bacbeee4d39
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2013-11-27 17:08:17 +0700

    revert

--- web.js
+++ web.js
@@ -4,7 +4,4 @@ var app = express();
  
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'));
-console.log(app.get('port'));
+app.listen(process.env.PORT || 5000);
