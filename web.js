f53e8b047afc3cbb35313557f7aba942bbf1f50e
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2013-11-29 18:47:11 +0700

    update

--- web.js
+++ web.js
@@ -4,4 +4,4 @@ var app = express();
  
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
