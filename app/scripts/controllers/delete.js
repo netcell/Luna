fbe9e3f35a91ffb29de58447ee3fb051c5d1d5c4
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2013-12-14 16:44:56 +0700

    github

--- app/scripts/controllers/delete.js
+++ app/scripts/controllers/delete.js
@@ -25,7 +25,7 @@ angular.module('lunaApp')
 
     $scope.submit = function(){
     	if ($scope.email && Validate.validateEmail($scope.email)) {
-            User.setEmail($scope.selection.email);
+            User.setEmail($scope.email);
             $scope.main.createPopup('Đang xử lý');
             $http.get('/user/delete-event/'+$scope.email).then(function(res){
             	if (res.data == "0") {
