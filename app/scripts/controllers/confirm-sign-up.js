def35ad491d5b7f5090b614b415fb8fd60ad1254
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2014-01-07 14:27:53 +0700

    a

---
+++ app/scripts/controllers/confirm-sign-up.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lunaApp')
+  .controller('ConfirmSignUpCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
