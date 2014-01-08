fd05bf13d52d613b3cd2b0b709c74cb648aefdb6
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2014-01-08 20:28:36 +0700

    for master

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,10 +2,8 @@
 
 angular.module('lunaApp')
   .controller('HomeCtrl', function ($scope,$location,User) {
-    $scope.main.createPopup('Đang xử lý');
     User.signIn(function(){
         if (User.getInfo().signedIn) $location.path('/event-list');
-        $scope.main.closePopup();
     },true);
     $scope.footer.buttons = [
     	{
