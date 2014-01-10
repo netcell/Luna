ddffa54a9b21a4def3735a100714dbdb8cca3f06
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2014-01-10 19:35:13 +0700

    update

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -2,16 +2,16 @@
 
 angular.module('lunaApp')
   .controller('AccountCtrl', function ($scope,$location,User) {
-    // $scope.main.createPopup('Đang xử lý');
-    // User.signIn(function(exitCode){
-    //     if (exitCode) {
-    //         $scope.User = User.getInfo();
-    //         $scope.main.closePopup();
-    //     } else {
-    //         $location.path('/sign-in');
-    //         $scope.main.alert('Có lỗi trong quá trình đăng nhập. Xin hãy thử lại sau.');
-    //     }
-    // })
+    $scope.main.createPopup('Đang xử lý');
+    User.signIn(function(exitCode){
+        if (exitCode) {
+            $scope.User = User.getInfo();
+            $scope.main.closePopup();
+        } else {
+            $location.path('/sign-in');
+            $scope.main.alert('Có lỗi trong quá trình đăng nhập. Xin hãy thử lại sau.');
+        }
+    })
     $scope.footer.buttons = [
     	{
     		name:'danh sách nhắc nhở',
