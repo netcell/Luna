15bac78a1272fa85e21c5101328d7df2f304cebe
Nguyễn Tuấn Anh <anhnt.fami@gmail.com>
2014-01-15 20:03:21 +0700

    update

--- app/scripts/controllers/LoadingScreen.js
+++ app/scripts/controllers/LoadingScreen.js
@@ -12,4 +12,19 @@ angular.module('lunaApp')
         $http.get('views/has.html')
     ];
     $('.loading-screen').fadeOut(500);
+    $( document ).on(
+    "click",
+    "a",
+    function( event ){
+     
+    // Stop the default behavior of the browser, which
+    // is to change the URL of the page.
+    event.preventDefault();
+     
+    // Manually change the location of the page to stay in
+    // "Standalone" mode and change the URL at the same time.
+    location.href = $( event.target ).attr( "href" );
+     
+    }
+    );
   });
